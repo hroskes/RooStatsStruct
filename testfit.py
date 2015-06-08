945918c556b571eefb48c0b38540eda3662c3450
Heshy Roskes <jroskes1@jhu.edu>
2015-06-09 01:22:07 +0200

    Try to make plots with mu

--- testfit.py
+++ testfit.py
@@ -9,20 +9,25 @@ f = ROOT.TFile.Open("fa3_0_2_0_workspace.root")
 w = f.Get("workspace")
 
 fa3 = w.var("fa3")
+mu = w.var("mu")
 
 pdf = w.pdf("Signal_0_2_0_SumPDF")
 sMELA = w.var("sMELA")
 D0minus = w.var("D0-_VBF")
 DCP = w.var("Dcp_VBF")
 
+testmu = 0
 testfa3 = 0
 ntoys = 1000000
+
+mu.setVal(testmu)
 
 fa3.setVal(0)
 data = pdf.generate(ROOT.RooArgSet(sMELA, D0minus, DCP), ntoys*(1-testfa3))
 fa3.setVal(1)
 data.append(pdf.generate(ROOT.RooArgSet(sMELA, D0minus, DCP), ntoys*testfa3))
 
+#mu.setVal(random.random())
 #fa3.setVal(random.random())
 #pdf.fitTo(data)
 
@@ -37,5 +42,3 @@ frame.Draw()
 #frame.GetYaxis().SetRangeUser(0, frame.GetYaxis().GetXmax())
 frame.GetYaxis().SetRangeUser(0, 10**5)
 c1.SaveAs("/afs/cern.ch/user/h/hroskes/www/TEST/test.png")
-
-
