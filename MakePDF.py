7c7eca40ce924a14b5ed98e0552b5785ca84e6a4
Heshy Roskes <jroskes1@jhu.edu>
2015-06-09 01:42:13 +0200

    Fix spelling

--- MakePDF.py
+++ MakePDF.py
@@ -161,9 +161,9 @@ class MakePDF:
 		TemplateName = "Signal_{0}_{1}_{2}_SumPDF".format(self.channel,self.category,self.on_off)
 		SignalPDF = ROOT.RooRealSumPdf(TemplateName, TemplateName, ROOT.RooArgList(SMhistFunc, MIXhistFunc, PShistFunc), ROOT.RooArgList(SMnorm,MIXnorm,PSnorm))
 		
-		TempalteName = "Signal_{0}_{1}_{2}_norm".format(self.channel,self.category,self.on_off)
+		TemplateName = "Signal_{0}_{1}_{2}_norm".format(self.channel,self.category,self.on_off)
 		#NOTE BELOW INCLUDES MU AND SMrate Below NOT COMBINE COMPATABLE
-		SIGnorm = ROOT.RooFormulaVar(TempalteName, "@6*@5*((1-abs(@0))+abs(@0)*@1 +(@0>0 ? 1.: -1.)*sqrt (abs(@0)*(1-abs(@0)))*(cos(@4)*@2 +sin(@4)*@3))",RooArgList(fa3, r1, r2, r3, phi, mu, SMrate))
+		SIGnorm = ROOT.RooFormulaVar(TemplateName, "@6*@5*((1-abs(@0))+abs(@0)*@1 +(@0>0 ? 1.: -1.)*sqrt (abs(@0)*(1-abs(@0)))*(cos(@4)*@2 +sin(@4)*@3))",RooArgList(fa3, r1, r2, r3, phi, mu, SMrate))
 		TemplateName = "Total_{0}_{1}_{2}_SumPDF".format(self.channel,self.category,self.on_off)
 		TotalPDF = ROOT.RooRealSumPdf(TemplateName, TemplateName, ROOT.RooArgList(SignalPDF,BKGhistFunc),ROOT.RooArgList(SIGnorm,BKGrate))
 
