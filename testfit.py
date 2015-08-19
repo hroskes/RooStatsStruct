1b6ddf72f367dc98e6dc850e588c1ee555f5657b
Heshy Roskes <jroskes1@jhu.edu>
2015-08-19 17:00:59 +0200

    Found it like this

--- testfit.py
+++ testfit.py
@@ -14,7 +14,7 @@ nbins = 1000
 low = -1
 high = 1
 testmu = 1
-testfa3 = 0
+testfa3 = -0.5
 ntoys = None  #default: BKGrate+SMrate
 ########################################
 
@@ -30,7 +30,7 @@ D0minus = w.var("D0-_VBF")
 DCP = w.var("Dcp_VBF")
 
 if ntoys is None:
-    ntoys = w.var("BKGrate").getVal() + w.var("SMrate").getVal()
+    ntoys = (w.var("BKGrate").getVal() + w.var("SMrate").getVal())
 
 print "Number of toys:", ntoys
 
@@ -45,8 +45,6 @@ multigraph = ROOT.TMultiGraph()
 
 averageNLL = ExtendedCounter()
 for j in range(nNLLs):
-    if j % 25 == 0:
-        print str(j) + " / " + str(nNLLs)
     mu.setVal(testmu)
     fa3.setVal(testfa3)
     data = pdf.generate(ROOT.RooArgSet(sMELA, D0minus, DCP), ntoys)
@@ -60,6 +58,8 @@ for j in range(nNLLs):
     graph = result.TGraph()
     graph.SetLineColor(17)
     multigraph.Add(graph)
+    if (j+1) % 25 == 0 or (j+1) == nNLLs:
+        print str(j+1) + " / " + str(nNLLs)
 
 averageNLL /= nNLLs
 
@@ -70,9 +70,8 @@ multigraph.Add(graph)
 multigraph.Draw("AC")
 multigraph.GetYaxis().SetTitle("-2#Deltaln L")
 multigraph.GetXaxis().SetTitle("f_{a_{3}}")
-#frame.GetYaxis().SetRangeUser(0, frame.GetYaxis().GetXmax())
+multigraph.GetYaxis().SetRangeUser(0,.003)
 
-style.style()
 style.drawlines()
 
-c1.SaveAs("/afs/cern.ch/user/h/hroskes/www/VBF/Summer2015/scans/fa3=%s.png" % testfa3)
+[c1.SaveAs("/afs/cern.ch/user/h/hroskes/www/VBF/Summer2015/scans/fa3=%s_zoomed.%s" % (testfa3, format)) for format in ["png", "eps", "root", "pdf"]]
