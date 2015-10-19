e298939c8722664e12dc792cd417650d300dfe39
Christopher Martin <chmartin@cern.ch>
2015-10-19 14:19:55 +0200

    Add functions for category norms

--- MakePDF_Heshy.py
+++ MakePDF_Heshy.py
@@ -14,7 +14,7 @@ class MakePDF_Heshy(MakePDF.MakePDF):
             self.makeWorkspace(channel)
 
     def makeWorkspace(self, channelCode):
-        categoryCode = 2
+        #categoryCode = 2
         templateFile = templatedir + channelnames[channelCode] + "_templates.root"
         templateFile_bkg = templatedir + channelnames[channelCode] + "_templates_bkg.root"
         on_off_Code = 0
@@ -22,7 +22,7 @@ class MakePDF_Heshy(MakePDF.MakePDF):
         templateName_PS = "template_VBFpseudoscalar"
         templateName_mix = "template_VBFinterference"
         templateName_bkg = "template_qqZZ"
-        MakePDF.MakePDF.makeWorkspace(self, channelCode, categoryCode, templateFile, templateFile_bkg, on_off_Code, templateName_SM, templateName_PS, templateName_mix, templateName_bkg)
+        MakePDF.MakePDF.makeWorkspace(self, channelCode, templateFile, templateFile_bkg, on_off_Code, templateName_SM, templateName_PS, templateName_mix, templateName_bkg)
 
 if __name__ == '__main__':
     MakePDF.turnoffbkg = True
