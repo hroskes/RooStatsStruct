c075d17ef23db7dbae3a2c80e85bd7967ecacb0e
Heshy Roskes <jroskes1@jhu.edu>
2015-11-23 17:51:57 +0100

    Fix project for decay

--- testproject.py
+++ testproject.py
@@ -11,20 +11,21 @@ import style
 #parameters
 testmu = 1
 testfa3s = {0: 1, 1: 2, 0.5: 4, -0.5: 418}
-varnames = ["sMELA", "D0-_VBF", "Dcp_VBF"]
+varnames = ["sMELA_ggH", "D0-_dec", "Dcp_dec"]
 ########################################
 
-f = ROOT.TFile.Open("fa3_0_2_0_workspace_nobkg.root")
-#f = ROOT.TFile.Open("fa3_0_2_0_workspace.root")
+f = ROOT.TFile.Open("fa3_0_0_workspace_nobkg.root")
+#f = ROOT.TFile.Open("fa3_0_0_workspace.root")
 w = f.Get("workspace")
 
 fa3 = w.var("fa3")
 mu = w.var("mu")
 
-pdf = w.pdf("Total_0_2_0_SumPDF")
+pdf = w.pdf("ggH_0_0")
 
 for varname in varnames:
     var = w.var(varname)
+    print varname, var
     frame = var.frame()
 
     othervarnames = varnames[:]
