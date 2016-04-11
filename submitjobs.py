84166b8ddaaa5bc9e30d688239a9a4953c3dad7a
Heshy Roskes <jroskes1@jhu.edu>
2016-04-11 15:32:27 -0400

    Fix job submission on MARCC

--- submitjobs.py
+++ submitjobs.py
@@ -5,6 +5,18 @@ import pipes
 import subprocess
 import textwrap
 
+def preparetemplates():
+    subprocess.check_call("""
+      cd templates/VBF &&
+      python makefinaltemplates.py
+    """, shell = True)
+    subprocess.check_call("""
+      cd templates/ggH_fromMeng_normalized &&
+      root -l -b -q normalizesig.C+ &&
+      root -l -b -q normalizebkg.C+
+    """, shell = True)
+    
+
 def bsub_CMS(job, jobname, repmap):
     repmap.update(
                   CMSSW = os.environ["CMSSW_BASE"],
@@ -21,20 +33,33 @@ def bash(job, jobname, repmap):
     subprocess.check_call(job.format(**repmap), shell=True)
 
 def sbatch(job, jobname, repmap):
+    if "ggH" in str(whichtemplates):
+         memory = 10000
+         partition = "lrgmem"
+    else:
+         memory = 3000
+         partition = "shared"
+
     repmap.update(
                   CMSSW = os.environ["CMSSW_BASE"],
-                  job = job,
                   jobname = jobname,
+                  memory = memory,
+                  partition = partition,
                  )
     job = job.format(**repmap)
+    repmap.update(
+                  job = job
+                 )
+
+
     jobfile = textwrap.dedent("""\
     #!/bin/bash
     #SBATCH --job-name={jobname}
     #SBATCH --time=24:0:0
     #SBATCH --nodes=1
     #SBATCH --ntasks-per-node=1
-    #SBATCH --partition=shared
-    #SBATCH --mem=3000
+    #SBATCH --partition={partition}
+    #SBATCH --mem={memory}
 
     . /work-zfs/lhc/cms/cmsset_default.sh
     cd {CMSSW} &&
@@ -42,7 +67,7 @@ def sbatch(job, jobname, repmap):
     cd $SLURM_SUBMIT_DIR &&
     echo "SLURM job running in: " `pwd` &&
     {job}
-    """).format(job=job, jobname=jobname, CMSSW = os.environ["CMSSW_BASE"])
+    """).format(**repmap)
 
     with open("slurm.sh", "w") as f:
         f.write(jobfile)
@@ -63,8 +88,10 @@ job = """
 cd {pwd} &&
 python MakePDF.py {templates} &&
 python testproject.py {templates} &&
-python testfit.py {templates}
+python testfit.py {templates} | grep -v "Integral below threshold: 0$" | grep -v "hobs is not found"
 """
+
+preparetemplates()
 
 for whichtemplates in enums.WhichTemplates.enumitems:
     repmap = {
