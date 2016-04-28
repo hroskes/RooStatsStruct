b899f124c391b96b5cf913bfaf66cd25824f4628
Heshy Roskes <jroskes1@jhu.edu>
2016-04-28 13:25:47 -0400

    Cross sections for ggH

--- templates/VBF/samples.py
+++ templates/VBF/samples.py
@@ -32,16 +32,20 @@ class Sample(object):
         return result
 
     def crosssection(self):
-        if self.productionmode == "VBF":
+        if self.productionmode == "ggH":
+            SMXS = constants.SMXS_ggH
+            SMBR = constants.SMBR
+            return SMXS * SMBR * (self.JHUcrosssection() / constants.JHU_XS_a1_ggH)
+        elif self.productionmode == "VBF":
             SMXS = constants.SMXS_VBF
             SMBR = constants.SMBR
-        try:
             return SMXS * SMBR * (self.JHUcrosssection() / (constants.JHU_XS_a1_VBF*constants.JHU_XS_a1_ggH))
-        except NameError:
-            raise NotImplementedError("Information for %s not put into samples.Sample.crosssection!"%self)
+        raise NotImplementedError("Information for %s not put into samples.Sample.crosssection!"%self)
 
     def JHUcrosssection(self):
-        if self.productionmode == "VBF":
+        if self.productionmode == "ggH":
+             return self.g12g40xs() + self.g11g41xs() + self.g10g42xs()
+        elif self.productionmode == "VBF":
             return self.g14g40xs() + self.g13g41xs() + self.g12g42xs() + self.g11g43xs() + self.g10g44xs()
         assert False
 
@@ -49,7 +53,7 @@ class Sample(object):
         if self.productionmode == "VBF":
             if self.hypothesis == "0-":
                 return 1.0
-            if self.hypothesis == "0+":
+            elif self.hypothesis == "0+":
                 return 0.0
             elif self.hypothesis == "fa30.5":
                  return .5
@@ -65,6 +69,16 @@ class Sample(object):
                 return -0.9
         assert False
 
+    def fa3HZZ(self):
+        if self.productionmode == "ggH":
+            if self.hypothesis == "0-":
+                return 1.0
+            elif self.hypothesis == "0+":
+                return 0.0
+            elif self.hypothesis == "fa30.5":
+                return 0.5
+        assert False
+
     def calcg1g4(self):
         if self.productionmode == "VBF":
             if self.hypothesis == "0-":
@@ -78,6 +92,18 @@ class Sample(object):
             self.__g1 /= divideby**.25
             self.__g4 /= divideby**.25
 
+        elif self.productionmode == "ggH":
+            if self.hypothesis == "0-":
+                self.__g1 = 0
+                self.__g4 = (constants.JHU_XS_a1_ggH / constants.JHU_XS_a3_ggH)**.5
+            else:
+                self.__g1 = 1
+                self.__g4 = copysign(sqrt(abs(self.fa3HZZ())*constants.JHU_XS_a1_ggH / ((1-abs(self.fa3HZZ()))*constants.JHU_XS_a3_ggH)), self.fa3HZZ())
+
+            divideby = (self.JHUcrosssection() / constants.JHU_XS_a1_ggH)
+            self.__g1 /= divideby**.5
+            self.__g4 /= divideby**.5
+
         try:
             self.__g1, self.__g4
         except AttributeError:
@@ -89,12 +115,35 @@ class Sample(object):
         return self.__g4
 
     def g14g40xs(self):
-        return self.__g1**4 * constants.JHU_XS_a1_VBF*constants.JHU_XS_a1_ggH
+        if self.productionmode == "VBF":
+            return self.__g1**4 * constants.JHU_XS_a1_VBF*constants.JHU_XS_a1_ggH
+        assert False
     def g13g41xs(self):
-        return 0
+        if self.productionmode == "VBF":
+            return 0
+        assert False
     def g12g42xs(self):
-        return self.__g1**2*self.__g4**2 * (constants.JHU_XS_a1_VBF*constants.JHU_XS_a3_ggH + constants.JHU_XS_a3_VBF*constants.JHU_XS_a1_ggH)
+        if self.productionmode == "VBF":
+            return self.__g1**2*self.__g4**2 * (constants.JHU_XS_a1_VBF*constants.JHU_XS_a3_ggH + constants.JHU_XS_a3_VBF*constants.JHU_XS_a1_ggH)
+        assert False
     def g11g43xs(self):
-        return 0
+        if self.productionmode == "VBF":
+            return 0
+        assert False
     def g10g44xs(self):
-        return self.__g4**4 * constants.JHU_XS_a3_VBF*constants.JHU_XS_a3_ggH
+        if self.productionmode == "VBF":
+            return self.__g4**4 * constants.JHU_XS_a3_VBF*constants.JHU_XS_a3_ggH
+        assert False
+
+    def g12g40xs(self):
+        if self.productionmode == "ggH":
+            return self.__g1**2 * constants.constants.JHU_XS_a1_ggH
+        assert False
+    def g11g41xs(self):
+        if self.productionmode == "ggH":
+            0
+        assert False
+    def g10g42xs(self):
+        if self.productionmode == "ggH":
+            return self.__g4**2 * constants.constants.JHU_XS_a3_ggH
+        assert False
